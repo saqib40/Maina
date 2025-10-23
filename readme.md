b53417d51d49fb5f1989298f4556a4b9da2a5a97
saqib <realhussainsakib@gmail.com>
2025-10-23 09:28:36 +0000

    added readme

---
+++ readme.md
@@ -0,0 +1,3 @@
+# Maina
+### Cuase we all love Nayaab
+Maina is a simple, peer-to-peer video calling application built with React and WebRTC. It allows two users to connect directly without a server by manually exchanging connection details (SDP and ICE candidates).
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import CreateCallPage from "./pages/CreateCallPage";
 import HomePage from "./pages/HomePage";
 import JoinCallPage from "./pages/JoinCallPage";
-import { Route, BrowserRouter, Routes } from 'react-router-dom'
+import { Route, HashRouter, Routes } from 'react-router-dom'
 
 export default function App() {
 
@@ -10,13 +10,13 @@ export default function App() {
       <div className="absolute inset-0 bg-cover bg-center" style={{backgroundImage: "url('https://placehold.co/1920x1080/282c34/282c34?text=.')"}}></div>
       <div className="absolute inset-0 bg-gray-800/80 backdrop-blur-md"></div>
       <div className="relative z-10 w-full flex items-center justify-center min-h-screen">
-        <BrowserRouter>
+        <HashRouter>
           <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/create" element={<CreateCallPage />} />
             <Route path="/join" element={<JoinCallPage />} />
           </Routes>
-        </BrowserRouter>
+        </HashRouter>
       </div>
     </main>
   );
